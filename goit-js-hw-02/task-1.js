769efec8ac82e36b2a33139390f1ff1ed34e4c5a
Andrey Hryhoriev <andreygrigoryev@ukr.net>
2020-04-30 16:40:50 +0300

    new commit

--- goit-js-hw-02/task-1.js
+++ goit-js-hw-02/task-1.js
@@ -1,28 +1,7 @@
-// Напиши функцию logItems(array), которая получает массив и использует цикл for, который для каждого 
-// элемента массива будет выводить в консоль сообщение в формате [номер элемента] - [значение элемента].
-
-// Нумерация должна начинаться с 1. К примеру для первого элемента массива ['Mango', 'Poly', 'Ajax'] 
-// с индексом 0 будет выведено '1 - Mango', а для индекса 2 выведет '3 - Ajax'.
-
-// const logItems = function(array) {
-//     // твой код
-//   };
-  
-//   /*
-//    * Вызовы функции для проверки работоспособности твоей реализации.
-//    */
-//   logItems(['Mango', 'Poly', 'Ajax', 'Lux', 'Jay', 'Kong']);
-  
-//   logItems([5, 10, 15, 20, 25, 30, 35, 40, 45, 50]);
-
-
-const logItems = function(){
-    let total=0;
-    for(argument of arguments){
-        total=+argument
-    }
-    return total
-    console.log(total)
+const logItems = function(array){
+  for(let i=0; i<array.length; i+=1){
+    console.log(`${i+1}-${array[i]}`)
+  }
 }
 
 logItems(['Mango', 'Poly', 'Ajax', 'Lux', 'Jay', 'Kong']);
@@ -30,4 +9,3 @@ logItems([5, 10, 15, 20, 25, 30, 35, 40, 45, 50]);
 
 
 
-logItems
