a9c347395fa73e6e6b97c4dc88aaea0b8fa51be2
Andrey Hryhoriev <andreygrigoryev@ukr.net>
2020-06-15 22:28:11 +0300

    7hw 08-task

--- goit-js-hw-07/task-8.js
+++ goit-js-hw-07/task-8.js
@@ -22,8 +22,8 @@ const renderRef = document.querySelector('button[data-action="render"]');
 const destroyRef = document.querySelector('button[data-action="destroy"]');
 boxesRef.append(newBox);
 
-renderRef.addEventListener('click');
-destroyRef.addEventListener('click');
+// renderRef.addEventListener('click');
+// destroyRef.addEventListener('click');
 
 // function randColor() {
 //   const r = Math.floor(Math.random() * 256);
@@ -32,3 +32,50 @@ destroyRef.addEventListener('click');
 //   const bgColor = `${r},${g},${b}`;
 //   return bgColor;
 // }
+
+// const controlsRef = document.querySelector('#controls');
+// const inputRef = controlsRef.firstElementChild;
+// const createBtnRef = controlsRef.querySelector('button[data-action="render"]');
+// const clearBtnRef = controlsRef.querySelector('button[data-action="destroy"]');
+// const boxesRef = document.querySelector('#boxes');
+// let width = 30;
+// let height = 30;
+// let boxItemsRef = [];
+
+// const randomBgColor = () => {
+//   const r = Math.floor(Math.random() * 256);
+//   const g = Math.floor(Math.random() * 256);
+//   const b = Math.floor(Math.random() * 256);
+//   const bgColor = `rgb(${r},${g},${b})`;
+//   return bgColor;
+// };
+
+// const createBox = () => {
+//   const boxRef = document.createElement('div');
+//   boxRef.style.width = `${width}px`;
+//   boxRef.style.height = `${height}px`;
+//   boxRef.style.backgroundColor = randomBgColor();
+//   return boxRef;
+// };
+
+// const createBoxes = amount => {
+//   for (let i = 0; i < amount; i += 1) {
+//     boxItemsRef.push(createBox());
+//     width += 10;
+//     height += 10;
+//   }
+//   boxesRef.append(...boxItemsRef);
+// };
+
+// createBtnRef.addEventListener('click', () => {
+//   createBoxes(Number(inputRef.value));
+// });
+
+// clearBtnRef.addEventListener('click', () => {
+//   while (boxesRef.firstChild) {
+//     boxesRef.firstChild.remove();
+//   }
+//   boxItemsRef = [];
+//   width = 30;
+//   height = 30;
+// });
